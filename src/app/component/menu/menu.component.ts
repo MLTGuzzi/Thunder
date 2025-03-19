b68d61b2cbc6a019d3bd9e399150dc9e6f8cf16b
Martin Turner <MLTGuzzi@hotmail.com>
2025-03-19 19:27:52 -0400

    Added the spreadsheet with the user stories. Now using Material for responsive design.  When in small form factor mode, the menu collapses into a hamburger.

--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
   standalone: true,
-  imports: [CommonModule, RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
 })
 export class MenuComponent {
   constructor(public auth: AuthService) {}
