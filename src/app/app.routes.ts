e2add8f25e805d947a8e1f6c00a10bd662d218e7
Martin Turner <MLTGuzzi@hotmail.com>
2025-04-07 22:30:30 -0400

    Created work-order-list component and added to the dashboard.  Needs to be tied into data.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { WorkOrderListComponent } from './component/work-order-list/work-order-list.component';
 import { CheckInComponent } from './component/check-in/check-in.component';
 
 export const routes: Routes = [
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,10 +3,17 @@ import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarRow } from '@angular/material/toolbar';
+import { WorkOrderListComponent } from '../../component/work-order-list/work-order-list.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ RouterModule, MatSidenavModule, MatListModule, MatToolbarRow ],
+  imports: [ 
+    RouterModule, 
+    MatSidenavModule, 
+    MatListModule, 
+    MatToolbarRow,
+    WorkOrderListComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
